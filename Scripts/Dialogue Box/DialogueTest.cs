bf942b0c30c2288e3f3b5300fd540c17d2980ce2
Abdalla Haider Abbas <abendos203@gmail.com>
2026-09-15 03:55:26 -0300

    Added panels
    added custom panels we can use in the future

--- Scripts/Dialogue Box/DialogueTest.cs
+++ Scripts/Dialogue Box/DialogueTest.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class DialogueTest : CanvasLayer
 {
+	private PackedScene _SLACKING = GD.Load<PackedScene>("res://Scenes/SLACKING.tscn");
+	private bool hasSlacked = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -14,9 +16,13 @@ public partial class DialogueTest : CanvasLayer
         		if (Input.IsKeyPressed(Key.P) &&
 			Input.IsKeyPressed(Key.I) &&
 			Input.IsKeyPressed(Key.Z) &&
-			Input.IsKeyPressed(Key.A))
+			Input.IsKeyPressed(Key.A) &&
+			!hasSlacked)
 		{
-			GetNode<DialogueSystem>("Control").ShowMessage("stop slacking and deliver some god damn pizzas man.");	
+			Node2D SlackingInstance = _SLACKING.Instantiate<Node2D>();
+			GetTree().CurrentScene.AddChild(SlackingInstance);
+			GetNode<DialogueSystem>("Control").ShowMessage("stop slacking and deliver some god damn pizzas man.");
+			hasSlacked = true;
 		}
     }
 
